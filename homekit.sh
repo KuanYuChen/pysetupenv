e5fd45aae338df392afb2947bb76d28d8a8a5414
kuanyu chen <tasean833@gmail.com>
2017-07-13 17:31:10 +0800

    homekit

---
+++ homekit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install --yes nodejs
+
+sudo apt-get install libavahi-compat-libdnssd-dev
+
+sudo npm install -g homebridge
+sudo npm install -g homebridge-fakebulb
+mkdir ~/.homebridge
+cp config.json ~/.homebridge 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 sudo update-rc.d ssh enable
+sudo apt-get update 
 sudo apt-get install vim samba ntpdate git
 sudo apt-get install python3 python3-pip
 sudo smbpasswd -a pi
 sudo rm /etc/localtime
 sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
+sudo cp dhcpcd.conf /etc/dhcpcd.conf
+sudo cp smb.conf /etc/smb.conf
 
